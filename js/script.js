4924bf35daa53d7ad2f72175c952dda0d2f78b5f
unknown <insired@gmail.com>
2022-10-02 13:28:04 +0200

    aboutus

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const nav = document.querySelector('#navbar')
+	const allNavItems = document.querySelectorAll('.nav-link')
+	const navList = document.querySelector('.navbar-collapse')
 
 	const addShadow = () => {
 		window.scrollY >= 200 ? nav.classList.add('shadow-bg') : nav.classList.remove('shadow-bg')
 	}
+
+	allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
 
 	window.addEventListener('scroll', addShadow)
 })
